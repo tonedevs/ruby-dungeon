df77f02e7d2f4f359b7eeef81c6a67684b133983
Tony Monda <ajmonda@gmail.com>
2020-09-16 23:26:34 -0400

    Doors lock and unlock with user interaction. Cache prevents reading current location.

--- client/src/components/PlayerNavigation/ExploreOptions/ExploreOptions.jsx
+++ client/src/components/PlayerNavigation/ExploreOptions/ExploreOptions.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export default function ExploreOptions(props) {
   return (
     <div>
-      <Link to={props.northLinkTo}><button>North</button></Link>
-      <Link to={props.southLinkTo}><button>South</button></Link>
-      <Link to={props.eastLinkTo}><button>East</button></Link>
-      <Link to={props.westLinkTo}><button>West</button></Link>
+      <Link to={props.northLinkTo}><button id="north" onClick={props.onClick}>North</button></Link>
+      <Link to={props.southLinkTo}><button id="south" onClick={props.onClick}>South</button></Link>
+      <Link to={props.eastLinkTo}><button id="east" onClick={props.onClick}>East</button></Link>
+      <Link to={props.westLinkTo}><button id="west" onClick={props.onClick}>West</button></Link>
     </div>
   )
 }
