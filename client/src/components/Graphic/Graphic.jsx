0b0372b421be4e8cb6d382e7809e9a22c1071be6
Tony Monda <ajmonda@gmail.com>
2020-09-18 19:17:57 -0400

    positioning of graphic elements

--- client/src/components/Graphic/Graphic.jsx
+++ client/src/components/Graphic/Graphic.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Graphic.css'
 
 export default function Graphic() {
   return (
-    <div>
-      
+    <div id="graphic">
+      <img alt="interior"
+        src="https://raw.githubusercontent.com/ajmonda/ruby-dungeon/development/client/src/assets/images/dungeon-bg.gif" />
     </div>
   )
 }
